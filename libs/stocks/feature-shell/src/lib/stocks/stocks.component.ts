d4d3b2590b5d7bd9154cf0d313a93fea396b093e
Ghildiyal <vivek.ghildiyal@t-mobile.com>
2020-02-12 14:36:54 +0530

    Changes for Task2

--- libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
+++ libs/stocks/feature-shell/src/lib/stocks/stocks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { PriceQueryFacade } from '@coding-challenge/stocks/data-access-price-query';
+import {timeperiodsInventory} from './util/stocks-util';
 
 @Component({
   selector: 'coding-challenge-stocks',
@@ -14,26 +15,31 @@ export class StocksComponent implements OnInit {
 
   quotes$ = this.priceQuery.priceQueries$;
 
-  timePeriods = [
-    { viewValue: 'All available data', value: 'max' },
-    { viewValue: 'Five years', value: '5y' },
-    { viewValue: 'Two years', value: '2y' },
-    { viewValue: 'One year', value: '1y' },
-    { viewValue: 'Year-to-date', value: 'ytd' },
-    { viewValue: 'Six months', value: '6m' },
-    { viewValue: 'Three months', value: '3m' },
-    { viewValue: 'One month', value: '1m' }
-  ];
-
+  // timePeriods = [
+  //   { viewValue: 'All available data', value: 'max' },
+  //   { viewValue: 'Five years', value: '5y' },
+  //   { viewValue: 'Two years', value: '2y' },
+  //   { viewValue: 'One year', value: '1y' },
+  //   { viewValue: 'Year-to-date', value: 'ytd' },
+  //   { viewValue: 'Six months', value: '6m' },
+  //   { viewValue: 'Three months', value: '3m' },
+  //   { viewValue: 'One month', value: '1m' }
+  // ];
+  timePeriods = timeperiodsInventory;
   constructor(private fb: FormBuilder, private priceQuery: PriceQueryFacade) {
-    this.stockPickerForm = fb.group({
-      symbol: [null, Validators.required],
-      period: [null, Validators.required]
-    });
+    // this.stockPickerForm = fb.group({
+    //   symbol: [null, Validators.required],
+    //   period: [null, Validators.required]
+    // });
   }
 
   ngOnInit() {
-    this.stockPickerForm.valueChanges.subscribe(this.fetchQuote);
+    this.stockPickerForm = this.fb.group({
+      symbol: [null, {validators: [Validators.required],updateOn: "blur"}],
+      period: [null, {validators: [Validators.required],updateOn: "blur"}]
+    });
+    this.stockPickerForm.valueChanges.subscribe(() => {
+      this.fetchQuote()});
   }
 
   fetchQuote() {
